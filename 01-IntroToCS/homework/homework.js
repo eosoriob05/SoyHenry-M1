f3beaa58aad607c24717e5147dcad8f2a2967cec
eosoriob05 <eosoriob05@gmail.com>
2023-07-31 23:27:36 -0500

    Homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,48 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
 
-function DecimalABinario(num) {}
+   var stringSeparado = num.split('');
+
+   var counter = 0;
+   
+   var sumBinario = 0;
+   
+   for (let i = stringSeparado.length - 1; i >= 0; i--) {
+   
+      let numberBinario = parseInt(stringSeparado[counter]);
+   
+      let productBinario = numberBinario * (2**(i));
+   
+      sumBinario =  sumBinario + productBinario;
+
+      counter++;
+   }
+
+   return sumBinario;
+}
+
+
+function DecimalABinario(num) {
+
+   var binarioArray = [];
+   
+   do {
+      let divisionDecimal = num / 2;
+
+      let restoDecimal = num % 2;
+
+      if (restoDecimal === 0){
+         binarioArray.unshift('0');
+      } else {
+         binarioArray.unshift('1');
+      }
+      
+      num = divisionDecimal;
+   } while (num >= 1);
+
+   return binarioArray.join('');
+}
 
 module.exports = {
    BinarioADecimal,
